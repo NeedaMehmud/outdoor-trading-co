cbd959af3d3eed0cdb556e30c48122ec6eee8a16
Needa Mehmud <needa.mehmud@gmail.com>
2021-08-25 14:19:52 -0400

    resolved issues with deployment

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,16 @@
 import React, { useState } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-
+import Dashboard from '../src/components/Dashboard/Dashboard';
+import Login from '../src/components/Login/Login';
+import Preferences from '../src/components/Prefrences/Prefrences';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import './App.css';
-import Dashboard from '../Dashboard/Dashboard';
-import Login from '../Login/Login';
-import Preferences from '../Preferences/Preferences';
-// import React from 'react';
-// import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
-// import './App.css';
-// import Home from './pages/Home';
+import Home from './pages/Home';
 
-// const client = new ApolloClient({
-//   uri: '/graphql',
-//   cache: new InMemoryCache(),
-// });
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
 
 function App() {
   const [token, setToken] = useState();
@@ -23,24 +20,25 @@ function App() {
   }
 
   return (
-    <div className="wrapper">
-      <h1>Application</h1>
-      <BrowserRouter>
-        <Switch>
-          <Route path="/dashboard">
-            <Dashboard />
-          </Route>
-          <Route path="/preferences">
-            <Preferences />
-          </Route>
-        </Switch>
-      </BrowserRouter>
-    </div>
-    // <ApolloProvider client={client}>
-    //   <div>
-    //       <Home />
-    //     </div>
-    // </ApolloProvider>
+
+    <ApolloProvider client={client}>
+      <div>
+        <Home />
+      </div>
+      <div className="wrapper">
+        <h1>Application</h1>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/dashboard">
+              <Dashboard />
+            </Route>
+            <Route path="/preferences">
+              <Preferences />
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      </div>
+    </ApolloProvider>
   );
 }
 
