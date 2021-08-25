91f92bb9040d8a068d20ad80aa878a9d6d578e94
Needa Mehmud <needa.mehmud@gmail.com>
2021-08-25 12:42:18 -0400

    import login compmonnent and added a conditional statement

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,18 @@
-import logo from './logo.svg';
 import React, { useState } from 'react';
-import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import './App.css';
 import Dashboard from '../Dashboard/Dashboard';
+import Login from '../Login/Login';
 import Preferences from '../Preferences/Preferences';
 
 function App() {
   const [token, setToken] = useState();
+
+  if (!token) {
+    return <Login setToken={setToken} />
+  }
+
   return (
     <div className="wrapper">
       <h1>Application</h1>
