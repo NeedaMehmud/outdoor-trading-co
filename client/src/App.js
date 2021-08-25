cfcc0f5d401c52331ffc3e37492fcd58e6b026dd
Needa Mehmud <needa.mehmud@gmail.com>
2021-08-25 12:41:38 -0400

    imported useState

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Dashboard from '../Dashboard/Dashboard';
 import Preferences from '../Preferences/Preferences';
 
 function App() {
+  const [token, setToken] = useState();
   return (
     <div className="wrapper">
       <h1>Application</h1>
