cdf04346a448de321e052ecaa1fbefd27db9309a
Needa Mehmud <needa.mehmud@gmail.com>
2021-08-26 11:59:24 -0400

    added sign in css

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,3 +1,8 @@
+import "../style/SignIn.css";
+import React from "react";
+
+
+
 function Login() {
     return (
         <div class="login">
@@ -5,18 +10,18 @@ function Login() {
             <form class="login-container">
                 <div class="m-1">
                     <label for="exampleFormControlInput1" class="form-label">
-                        <b class="text-black">Username</b>
+                        <b class="text-white">Username</b>
                     </label>
                     <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
                 </div>
                 <div class="m-1">
                     <label for="exampleFormControlInput2" class="form-label">
-                        <b class="text-black">Password</b>
+                        <b class="text-white">Password</b>
                     </label>
                     <input type="email" class="form-control" id="exampleFormControlInput2" placeholder="Password" />
                 </div>
                 <div>
-                    <button type="submit" class="btn btn-secondary">Sign In</button>
+                    <button type="submit" class="signin-button">Sign In</button>
                 </div>
             </form>
         </div>
