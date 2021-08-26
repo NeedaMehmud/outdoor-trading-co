64f3e0821d7a71f12b43f331d2889b19ae0f671a
Needa Mehmud <needa.mehmud@gmail.com>
2021-08-26 13:17:24 -0400

    added modal to sign in

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,30 +1,46 @@
+import Modal from "react-bootstrap/Modal";
 import "../style/SignIn.css";
 import React from "react";
 
 
 
 function Login() {
+    const [isOpen, setIsOpen] = React.useState(false);
+    const showModal = () => {
+
+        setIsOpen(true);
+
+    };
+    const hideModal = () => {
+
+        setIsOpen(false);
+
+    };
     return (
-        <div class="login">
-            <div class="login-triangle"></div>
-            <form class="login-container">
-                <div class="m-1">
-                    <label for="exampleFormControlInput1" class="form-label">
-                        <b class="text-white">Username</b>
-                    </label>
-                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
-                </div>
-                <div class="m-1">
-                    <label for="exampleFormControlInput2" class="form-label">
-                        <b class="text-white">Password</b>
-                    </label>
-                    <input type="email" class="form-control" id="exampleFormControlInput2" placeholder="Password" />
-                </div>
-                <div>
-                    <button type="submit" class="rounded signin-button">Sign In</button>
-                </div>
-            </form>
-        </div>
+        <Modal.Body>
+            <div class="login">
+                <div class="login-triangle"></div>
+                <form class="login-container">
+                    <div class="m-1">
+                        <label for="exampleFormControlInput1" class="form-label">
+                            <b class="text-white">Username</b>
+                        </label>
+                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+                    </div>
+                    <div class="m-1">
+                        <label for="exampleFormControlInput2" class="form-label">
+                            <b class="text-white">Password</b>
+                        </label>
+                        <input type="email" class="form-control" id="exampleFormControlInput2" placeholder="Password" />
+                    </div>
+                    <Modal.Footer>
+                        <button onClick={showModal} type="submit" class="text-center rounded signin-button">Sign In</button>
+                        <Modal show={isOpen} onHide={hideModal} />
+                        <button class="text-center rounded signin-button" onClick={hideModal}>Cancel</button>
+                    </Modal.Footer>
+                </form>
+            </div>
+        </Modal.Body>
     );
 };
 
