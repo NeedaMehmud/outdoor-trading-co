54ec354d5325eff04e5126b895c36f0bfa8637b7
Needa Mehmud <needa.mehmud@gmail.com>
2021-08-27 11:35:50 -0400

    added space in login form

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -10,7 +10,6 @@ const Login = () => {
             <div class="login-triangle"></div>
             <form>
                 <h3>Log In</h3>
-
                 <div className="form-group login">
                     <label>Email address</label>
                     <input type="email" className="form-control" placeholder="Enter email" />
@@ -20,17 +19,19 @@ const Login = () => {
                     <label>Password</label>
                     <input type="password" className="form-control" placeholder="Enter password" />
                 </div>
-
                 <div className="form-group">
                     <div className="custom-control custom-checkbox">
                         <input type="checkbox" className="custom-control-input" id="customCheck1" />
                         <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
                     </div>
                 </div>
-                <button type="submit" className="rounded signin-button btn-block">Submit</button>
-                <p className="forgot-password text-right">
-                    Forgot <a href="#">password?</a>
-                </p>
+                <div class="d-grid gap-2 col-6 mx-auto">
+                    <br></br>
+                    <button type="submit" className="rounded signin-button btn-block">Submit</button>
+                    <p className="forgot-password text-right">
+                        Forgot <a href="#">password?</a>
+                    </p>
+                </div>
             </form>
         </div>
     );
