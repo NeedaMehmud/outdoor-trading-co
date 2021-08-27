54f9cafac7c186995c5ddbffa5af6cc4d2906a15
Needa Mehmud <needa.mehmud@gmail.com>
2021-08-27 19:02:43 -0400

    changed styling on login again

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -3,60 +3,34 @@ import "../style/Login.css";
 import React from "react";
 
 const Login = () => {
-  const [isOpen, setIsOpen] = React.useState(false);
-  const showModal = () => {
-    setIsOpen(true);
-  };
-  const hideModal = () => {
-    setIsOpen(false);
-  };
-  return (
-    <Modal.Body>
-      <div className="login">
-        <div className="login-triangle"></div>
-        <form className="login-container">
-          <div className="m-1">
-            <label for="exampleFormControlInput1" className="form-label">
-              <b className="text-white">Username</b>
-            </label>
-            <input
-              type="email"
-              className="form-control"
-              id="exampleFormControlInput1"
-              placeholder="name@example.com"
-            />
-          </div>
-          <div className="m-1">
-            <label for="exampleFormControlInput2" className="form-label">
-              <b className="text-white">Password</b>
-            </label>
-            <input
-              type="email"
-              className="form-control"
-              id="exampleFormControlInput2"
-              placeholder="Password"
-            />
-          </div>
-          <Modal.Footer>
-            <button
-              onClick={showModal}
-              type="submit"
-              className="text-center rounded signin-button"
-            >
-              Sign In
-            </button>
-            <Modal show={isOpen} onHide={hideModal} />
-            <button
-              className="text-center rounded signin-button"
-              onClick={hideModal}
-            >
-              Cancel
-            </button>
-          </Modal.Footer>
+    return (
+        <form>
+            <h3>Sign In</h3>
+
+            <div className="form-group">
+                <label>Email address</label>
+                <input type="email" className="form-control" placeholder="Enter email" />
+            </div>
+
+            <div className="form-group">
+                <label>Password</label>
+                <input type="password" className="form-control" placeholder="Enter password" />
+            </div>
+
+            <div className="form-group">
+                <div className="custom-control custom-checkbox">
+                    <input type="checkbox" className="custom-control-input" id="customCheck1" />
+                    <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
+                </div>
+            </div>
+
+            <button type="submit" className="btn btn-primary btn-block">Submit</button>
+            <p className="forgot-password text-right">
+                Forgot <a href="#">password?</a>
+            </p>
         </form>
-      </div>
-    </Modal.Body>
-  );
-};
+    );
+
+}
 
 export default Login;
