169172dec750768170a1f402f2cd4ffe3ccea4a2
Needa Mehmud <needa.mehmud@gmail.com>
2021-08-28 17:08:45 -0400

    worked on deubugging search returning duplicate genres

--- client/src/components/DropdownMenu.js
+++ client/src/components/DropdownMenu.js
@@ -2,15 +2,34 @@ import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import DropdownButton from 'react-bootstrap/DropdownButton';
 import Dropdown from 'react-bootstrap/Dropdown'
+import { gql, useQuery } from '@apollo/client';
 
-
+// Create genre query
+const GET_GENRES = gql`
+  query GetGenres {
+      items {
+          genre
+      }
+  }
+`;
 
 function DropdownMenu() {
     const [value, setValue] = useState('');
     const handleSelect = (e) => {
-        console.log(e);
         setValue(e)
     }
+
+    // run query
+    const { loading, error, data } = useQuery(GET_GENRES);
+
+    if (loading) return 'Loading...';
+    if (error) return `Error! ${error.message}`;
+
+    // get unique genres
+    const genres = data.items.map(item => item.genre)
+        .filter((value, index, self) => self.indexOf(value) === index);
+
+    genres.forEach(g => console.log(g));
 
     return (
         <div className="App container">
@@ -20,11 +39,12 @@ function DropdownMenu() {
                 id="dropdown-menu-align-right"
                 onSelect={handleSelect}
             >
-                <Dropdown.Item eventKey="option-1">option-1</Dropdown.Item>
-                <Dropdown.Item eventKey="option-2">option-2</Dropdown.Item>
-                <Dropdown.Item eventKey="option-3">option 3</Dropdown.Item>
-                <Dropdown.Divider />
-                <Dropdown.Item eventKey="some link">some link</Dropdown.Item>
+
+                {/* {create dropdown item per genre} */}
+                {genres.map(function(genre, _){
+                    return <Dropdown.Item eventKey={genre}>{genre}</Dropdown.Item>;
+                  })}
+
             </DropdownButton>
             <h4>You selected {value}</h4>
         </div>
