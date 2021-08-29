31df8903d0a39e6211737bf5a32b9585fe5f8cb0
Ren Ivey <haleyrivey@gmail.com>
2021-08-28 22:08:42 -0400

    Added button hovers. Added social media icons. Updated footer link styling.

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -10,12 +10,12 @@ function Footer() {
       <div className="container p-5">
         <div className="row p-3">
           <h3>Contact Us</h3>
-          <div className="col-3 sm-2">
+          <div className="col-2 sm-2">
             <p>1-800-OUT-DOOR</p>
             <p>1234 Pine Rd</p>
             <p>Outdoor, GA</p>
           </div>
-          <div className="col-3 sm-2">
+          <div className="col-2 sm-2">
             <ul>
               <li>
                 <a href="#">About</a>
@@ -28,22 +28,18 @@ function Footer() {
               </li>
             </ul>
           </div>
-          <div className="col-6 lg-1">
-            {" "}
-            <a
-              href="https://www.facebook.com/Outdoor-Trading-Co-100466249039342"
-              target="_blank"
-              alt="Facebook Logo"
-            >
-              <FontAwesomeIcon icon={faFacebook} />
-            </a>
-            <a
-              href="https://www.linkedin.com/company/79902606/admin/"
-              target="blank"
-              alt="LinkedIn Logo"
-            >
-              <FontAwesomeIcon icon={faLinkedinIn} />
-            </a>
+          <div className="col-8 lg-1">
+          <div className="footer-social-icons">
+    <h4 className="_14">Follow us on</h4>
+    <ul className="social-icons">
+        <li><a href="https://www.facebook.com/Outdoor-Trading-Co-100466249039342" className="social-icon"> <i className="fa fa-facebook"></i></a></li>
+        <li><a href="" className="social-icon"> <i className="fa fa-twitter"></i></a></li>
+        <li><a href="" className="social-icon"> <i className="fa fa-rss"></i></a></li>
+        <li><a href="" className="social-icon"> <i className="fa fa-youtube"></i></a></li>
+        <li><a href="https://www.linkedin.com/company/outdoor-trading-co/" className="social-icon"> <i className="fa fa-linkedin"></i></a></li>
+        <li><a href="" className="social-icon"> <i className="fa fa-github"></i></a></li>
+    </ul>
+</div>
           </div>
         </div>
       </div>
