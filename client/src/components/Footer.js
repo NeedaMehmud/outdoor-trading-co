b53b468d1635624088d7268948fb46b20d94c693
Ren Ivey <haleyrivey@gmail.com>
2021-08-29 16:09:45 -0400

    Styled footer. Created profile component.

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,45 +1,28 @@
 import React from "react";
 import "../style/Footer.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFacebook } from "@fortawesome/free-brands-svg-icons";
-import { faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
 
 function Footer() {
   return (
     <footer>
       <div className="container p-5">
-        <div className="row p-3">
+        <div className="row">
           <h3>Contact Us</h3>
-          <div className="col-3">
+          <div className="col-lg-4 col-sm-1 my-2">
             <p>1-800-OUT-DOOR</p>
             <p>1234 Pine Rd</p>
             <p>Outdoor, GA</p>
           </div>
-          <div className="col-3">
-            <ul>
-              <li>
-                <a href="#">About</a>
-              </li>
-              <li>
-                <a href="#">Team</a>
-              </li>
-              <li>
-                <a href="#">Link</a>
-              </li>
-            </ul>
+          <div className="col-lg-4 col-sm-1 my-2">
+            <p><a href="#">About</a></p>
+            <p><a href="#">Team</a></p>
+            <p><a href="#">Link</a></p>
           </div>
-          <div className="col-6">
-          <div className="footer-social-icons">
-    <h4 className="_14">Follow us on</h4>
-    <ul className="social-icons">
-        <li><a href="https://www.facebook.com/Outdoor-Trading-Co-100466249039342" className="social-icon"> <i className="fa fa-facebook"></i></a></li>
-        <li><a href="" className="social-icon"> <i className="fa fa-twitter"></i></a></li>
-        <li><a href="" className="social-icon"> <i className="fa fa-rss"></i></a></li>
-        <li><a href="" className="social-icon"> <i className="fa fa-youtube"></i></a></li>
-        <li><a href="https://www.linkedin.com/company/outdoor-trading-co/" className="social-icon"> <i className="fa fa-linkedin"></i></a></li>
-        <li><a href="" className="social-icon"> <i className="fa fa-github"></i></a></li>
-    </ul>
-</div>
+          <div className="col text-center">
+            <ul className="social-icons">
+              <li className="mx-2"><a href="https://www.facebook.com/Outdoor-Trading-Co-100466249039342" className="social-icon"> <i className="fa fa-facebook"></i></a></li>
+              <li className="mx-2"><a href="https://www.linkedin.com/company/outdoor-trading-co/" className="social-icon"> <i className="fa fa-linkedin"></i></a></li>
+              <li className="mx-2"><a href="https://github.com/NeedaMehmud2017/outdoor-trading-co" className="social-icon"> <i className="fa fa-github"></i></a></li>
+            </ul>
           </div>
         </div>
       </div>
