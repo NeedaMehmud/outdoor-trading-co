1436be6c275317588504f3545df243aeb9f5dd1f
Tyler Starling <tyler.a.starling@gmail.com>
2021-08-29 09:00:27 -0400

    trying to get cloudinary upload going

--- client/src/components/SubmitItemForm.js
+++ client/src/components/SubmitItemForm.js
@@ -1,137 +1,82 @@
 import React, { useState } from 'react';
-// import { Link } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
-
 import { ADD_ITEM } from '../utils/mutations';
-import { QUERY_ME } from '../utils/queries';
-
+import axios from 'axios'
 import Auth from '../utils/auth';
 
 const ItemForm = () => {
-    // genre will be drop down with given options
-    // const [item, setItem] = useState({ 
-    //     genre: "",
-    //     name: "",
-    //     location: "",
-    //     description: "",
-    //     condition: "",
-    //     image_id: "",
-    //     user: "",   
-    // }); 
+    const [fileState, setFileState] = useState({
+      file: [0]
+    });
 
-    // const [itemGenre, setItemGenre] = useState('');
-    // const [itemName, setItemName] = useState('');
-    // const [itemLocation, setItemLocation] = useState('');
-    // // condition will be drop down with given options
-    // const [itemCondition, setItemCondition] = useState(''); 
-    // const [itemDescription, setItemDescription] = useState(''); 
-    // const [itemImageId, setItemImageId] = useState(''); 
+    const formData = new FormData();
+    formData.append('file', fileState);
 
+    const handleFormSubmit = async (event) => {
+          event.preventDefault();
 
+      const response = await axios.post(
+      `https://api.cloudinary.com/v1_1/${process.env.REACT_APP_CLOUD_NAME}/image/upload`,
+      formData,
+    )
+    console.log(response);};
 
-    // const [addItem, { error }] = useMutation(ADD_ITEM, {
-    //     update(cache, { data: { addThought } }) {
-    //       try {
-    //         const { thoughts } = cache.readQuery({ query: QUERY_THOUGHTS });
+    return (
+      <form onSubmit={handleFormSubmit}>
+        <input name="image_id" type="file" accept="image/png, image/jpg, image/jpeg"></input>
+        <button type="submit">Submit</button>
+      </form>
+    );
+
+    // const [formState, setFormState] = useState({
+    //     name: '',
+    //     genre: '',
+    //     location: '',
+    //     description: '',
+    //     image_id:'',
+    //     condition:'',
+    // });
     
-    //         cache.writeQuery({
-    //           query: QUERY_THOUGHTS,
-    //           data: { thoughts: [addThought, ...thoughts] },
-    //         });
-    //       } catch (e) {
-    //         console.error(e);
-    //       }
-    
-    //       // update me object's cache
-    //       const { me } = cache.readQuery({ query: QUERY_ME });
-    //       cache.writeQuery({
-    //         query: QUERY_ME,
-    //         data: { me: { ...me, thoughts: [...me.thoughts, addThought] } },
-    //       });
-    //     },
-    //   });
+    //   // Set up our mutation with an option to handle errors
+    //   const [addItem, { error }] = useMutation(ADD_ITEM);
 
-
-    // const handleFormSubmit = async (event) => {
+    //   const handleFormSubmit = async (event) => {
     //     event.preventDefault();
     
+    //     // On form submit, perform mutation and pass in form data object as arguments
+    //     // It is important that the object fields are match the defined parameters in `ADD_THOUGHT` mutation
     //     try {
-    //       const { data } = await addItem({
-    //         variables: {
-    //           item,
-    //           user: Auth.getProfile().data.username,
+    //       const { data } = addItem({
+    //         variables: { 
+    //          ...formState,
+    //          user: Auth.getProfile().data.username,
     //         },
     //       });
-    
-    //       setItem({ 
-    //         genre: "",
-    //         name: "",
-    //         location: "",
-    //         description: "",
-    //         condition: "",
-    //         image_id: "",
-    //         user: "",   
-    //     });
+    //       console.log(formState);
+    //     //   window.location.reload();
     //     } catch (err) {
     //       console.error(err);
     //     }
     //   };
-
+    
     //   const handleChange = (event) => {
     //     const { name, value } = event.target;
-
-    //     setItem({...item, [name]: value})
-    //   }
-
-    const [formState, setFormState] = useState({
-        name: '',
-        genre: '',
-        location: '',
-        description: '',
-        image_id:'',
-        condition:'',
-      });
-    
-      // Set up our mutation with an option to handle errors
-      const [addItem, { error }] = useMutation(ADD_ITEM);
-
-      const handleFormSubmit = async (event) => {
-        event.preventDefault();
-    
-        // On form submit, perform mutation and pass in form data object as arguments
-        // It is important that the object fields are match the defined parameters in `ADD_THOUGHT` mutation
-        try {
-          const { data } = addItem({
-            variables: { 
-             ...formState,
-             user: Auth.getProfile().data.username,
-            },
-          });
-          console.log(formState);
-        //   window.location.reload();
-        } catch (err) {
-          console.error(err);
-        }
-      };
-    
-      const handleChange = (event) => {
-        const { name, value } = event.target;
-          setFormState({ ...formState, [name]: value });
-      };
+    //       setFormState({ ...formState, [name]: value });
+    //   };
     
     
 
-    return (
-        <form onSubmit={handleFormSubmit}>
-            <input name="name" type="text" onChange={handleChange}></input>
-            <input name="genre" type="text" onChange={handleChange}></input>
-            <input name="location" type="text" onChange={handleChange}></input>
-            <input name="condition" type="text" onChange={handleChange}></input>
-            <input name="description" type="text" onChange={handleChange}></input>
-            <input name="image_id" type="text" onChange={handleChange}></input>
-            <button type="submit">Submit</button>
-        </form>
-    );
+    // return (
+    //     <form onSubmit={handleFormSubmit}>
+    //         <input name="name" type="text" onChange={handleChange}></input>
+    //         <input name="genre" type="text" onChange={handleChange}></input>
+    //         <input name="location" type="text" onChange={handleChange}></input>
+    //         <input name="condition" type="text" onChange={handleChange}></input>
+    //         <input name="description" type="text" onChange={handleChange}></input>
+    //         <input name="image_id" type="text" onChange={handleChange}></input>
+    //         <button type="submit">Submit</button>
+    //     </form>
+    // );
 };   
     
 export default ItemForm;
