70285141c64caf98f4f8651e578983c602699bc9
Needa Mehmud <needa.mehmud@gmail.com>
2021-08-30 18:02:41 -0400

    styling icons wip

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -7,40 +7,40 @@ import { Link } from 'react-router-dom';
 
 function Footer() {
   return (
-    <footer class="footer">
+    <footer className="footer">
       <div className="footer-left col-md-4 col-sm-6">
-        <p class="about">
+        <p className="about">
           <h1> About Outdoor Trading Co.</h1>
           <p>Use our platform to rent/borrow outdoor gear and enjoy the outdoors.
           You can list your outdoor equipment up for rent/loan during the times you are unable to use it.
           Keep your equipment from gaining dust in the garage.
             Keep your outdoor activity list growing without the investments in outdoor equipment. </p>
         </p>
-        <div class="icons">
-          <a href="https://www.facebook.com/Outdoor-Trading-Co-100466249039342"><i class="fa fa-facebook"></i></a>
-          <a href="https://www.linkedin.com/company/outdoor-trading-co/"><i class="fa fa-linkedin"></i></a>
-          <a href="https://github.com/NeedaMehmud2017/outdoor-trading-co"><i class="fa fa-github"></i></a>
+        <div className="social-icons">
+          <a href="https://www.facebook.com/Outdoor-Trading-Co-100466249039342"><i className="fa fa-facebook"></i></a>
+          <a href="https://www.linkedin.com/company/outdoor-trading-co/"><i className="fa fa-linkedin"></i></a>
+          <a href="https://github.com/NeedaMehmud2017/outdoor-trading-co"><i className="fa fa-github"></i></a>
         </div>
       </div>
-      <div class="footer-center col-md-4 col-sm-6">
+      <div className="footer-center col-md-4 col-sm-6">
         <div>
-          <i class="fa fa-map-marker"></i>
-          <p><span> Street name and number</span> City, Country</p>
+          <i className="fa fa-map-marker"></i>
+          <p><span> 710 Outdoor Trading Street </span> Atlanta, Georgia</p>
         </div>
         <div>
-          <i class="fa fa-phone"></i>
-          <p> (+00) 0000 000 000</p>
+          <i className="fa fa-phone"></i>
+          <p> (+01) 123 789 9874</p>
         </div>
         <div>
-          <i class="fa fa-envelope"></i>
-          <p><a href="#"> office@company.com</a></p>
+          <i className="fa fa-envelope"></i>
+          <p><a href="#"> outdoortrading@company.com</a></p>
         </div>
       </div>
-      <div class="footer-right col-md-4 col-sm-6">
+      <div className="footer-right col-md-4 col-sm-6">
         <div className="navbar-brand">
           <img src={logo} alt="Outdoor Trading Co. logo" />
         </div>
-        <p class="menu">
+        <p className="menu">
           <a href="#"> Home</a> |
       <a href="#"> About</a> |
       <a href="#"> Services</a> |
@@ -48,11 +48,8 @@ function Footer() {
       <a href="#"> News</a> |
       <a href="#"> Contact</a>
         </p>
-        <p class="name"> Outdoor Trading Co. &copy; 2021</p>
+        <p className="name"> Outdoor Trading Co. &copy; 2021</p>
       </div>
-      {/* <div className="text-center p-3 copyright">
-        <p>Copyright 2021 Outdoor Trading Co.</p>
-      </div> */}
     </footer >
   );
 }
