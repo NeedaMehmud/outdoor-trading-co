be571576b9c03c884d98a416a34222dd23a4ac61
Lyndsie Lane <lyndsielane@yahoo.com>
2021-08-30 18:23:26 -0400

    fixed white-space

--- client/src/pages/ItemsInSingleGenre.js
+++ client/src/pages/ItemsInSingleGenre.js
@@ -31,7 +31,7 @@ function ItemsInSingleGenre(props) {
     return <div>Loading...</div>;
   }
 
-// if the user is not logged in then redirect to sign up page
+  // if the user is not logged in then redirect to sign up page
   if (!Auth.loggedIn()) {
     return <Redirect to="/SignUp" />;
   }
@@ -45,42 +45,42 @@ function ItemsInSingleGenre(props) {
       <Hero />
       <Iconbar />
       <main>
-      <div className="container">
-        {/* dynamic function allowing cards to be rendered for each item */}
-        {items.map((item) => (
-          <Card
-            className="card"
-            style={{ width: "25rem", height: "40rem" }}
-            key={item._id}
-          >
-            <Image
-              style={{ width: "25rem", height: "25rem" }}
-              variant="top"
-              className="img-fluid"
-              alt="Product image."
-              cloudName="outdoor-trading-co"
-              publicId={item.image_id}
-            />
-            <Card.Body>
-              <Card.Title class="card-title text-center title">
-                {item.name}
-              </Card.Title>
-            </Card.Body>
-            <ListGroup className="list-group-flush">
-              <ListGroupItem>Location: {item.location}</ListGroupItem>
-              <ListGroupItem>Condition: {item.condition}</ListGroupItem>
-            </ListGroup>
-            <Card.Body>
-              <Link to={`/${item._id}`}>
-                <button type="submit" className="request-btn">
-                  More Information
-                </button>
-              </Link>
-            </Card.Body>
-          </Card>
-        ))}
-      </div>
-      <br></br>
+        <div className="genre-container">
+          {/* dynamic function allowing cards to be rendered for each item */}
+          {items.map((item) => (
+            <Card
+              className="card"
+              style={{ width: "25rem", height: "40rem" }}
+              key={item._id}
+            >
+              <Image
+                style={{ width: "25rem", height: "25rem" }}
+                variant="top"
+                className="img-fluid"
+                alt="Product image."
+                cloudName="outdoor-trading-co"
+                publicId={item.image_id}
+              />
+              <Card.Body>
+                <Card.Title class="card-title text-center title">
+                  {item.name}
+                </Card.Title>
+              </Card.Body>
+              <ListGroup className="list-group-flush">
+                <ListGroupItem>Location: {item.location}</ListGroupItem>
+                <ListGroupItem>Condition: {item.condition}</ListGroupItem>
+              </ListGroup>
+              <Card.Body>
+                <Link to={`/${item._id}`}>
+                  <button type="submit" className="request-btn">
+                    More Information
+                  </button>
+                </Link>
+              </Card.Body>
+            </Card>
+          ))}
+        </div>
+        <br></br>
       </main>
     </div>
   );
