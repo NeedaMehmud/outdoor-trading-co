3f1d3fdcd021797b6e626c5b7ba9512f2080d2ab
Needa Mehmud <needa.mehmud@gmail.com>
2021-08-30 17:13:27 -0400

    added icons

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -13,17 +13,12 @@ function Footer() {
             Keep your equipment from gaining dust in the garage.
             Keep your outdoor activity list growing without the investments in outdoor equipment. </p>
           </p>
-          <div className="col-lg-4 col-sm-1 my-2">
-            <p><a href="#">About</a></p>
-            <p><a href="#">Team</a></p>
-            <p><a href="#">Link</a></p>
-          </div>
-          <div className="col text-center">
-            <ul className="social-icons">
-              <li className="m-2"><a href="https://www.facebook.com/Outdoor-Trading-Co-100466249039342" className="social-icon"> <i className="fa fa-facebook fa-lg"></i></a></li>
-              <li className="m-2"><a href="https://www.linkedin.com/company/outdoor-trading-co/" className="social-icon"> <i className="fa fa-linkedin fa-lg"></i></a></li>
-              <li className="m-2"><a href="https://github.com/NeedaMehmud2017/outdoor-trading-co" className="social-icon"> <i className="fa fa-github fa-lg"></i></a></li>
-            </ul>
+          <div class="icons">
+            <a href="https://www.facebook.com/Outdoor-Trading-Co-100466249039342"><i class="fa fa-facebook"></i></a>
+            <a href="https:twitter.com/"><i class="fa fa-twitter"></i></a>
+            <a href="https://www.linkedin.com/company/outdoor-trading-co/"><i class="fa fa-linkedin"></i></a>
+            <a href="https://github.com/NeedaMehmud2017/outdoor-trading-co"><i class="fa fa-github"></i></a>
+            <a href="https://www.instagram.com/"><i class="fa fa-instagram"></i></a>
           </div>
         </div>
       </div>
