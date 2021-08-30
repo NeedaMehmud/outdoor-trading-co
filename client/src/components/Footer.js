14db944f315985e37acd50a89efe609d3fb939b4
Needa Mehmud <needa.mehmud@gmail.com>
2021-08-30 17:24:12 -0400

    added company and street

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -20,6 +20,32 @@ function Footer() {
           </div>
         </div>
       </div>
+      <div class="footer-center col-md-4 col-sm-6">
+        <div>
+          <i class="fa fa-map-marker"></i>
+          <p><span> 123 Outdoor Trading Street</span> Atlanta, Georgia</p>
+        </div>
+        <div>
+          <i class="fa fa-phone"></i>
+          <p> (+01) 123-897-6932</p>
+        </div>
+        <div>
+          <i class="fa fa-envelope"></i>
+          <p><a href="#"> office@company.com</a></p>
+        </div>
+      </div>
+      <div class="footer-right col-md-4 col-sm-6">
+        <h2> Company<span> logo</span></h2>
+        <p class="menu">
+          <a href="#"> Home</a> |
+      <a href="#"> About</a> |
+      <a href="#"> Services</a> |
+      <a href="#"> Portfolio</a> |
+      <a href="#"> News</a> |
+      <a href="#"> Contact</a>
+        </p>
+        <p class="name"> Outdoor Trading Co. &copy; 2021</p>
+      </div>
       <div className="text-center p-3 copyright">
         <p>Copyright 2021 Outdoor Trading Co.</p>
       </div>
