7a3b4645fae2fae026ed0c232a2aafbdd81e50aa
Lyndsie Lane <lyndsielane@yahoo.com>
2021-08-30 11:58:03 -0400

    cards styled

--- client/src/pages/ItemsInSingleGenre.js
+++ client/src/pages/ItemsInSingleGenre.js
@@ -36,10 +36,11 @@ function ItemsInSingleGenre(props) {
         {items.map((item) => (
           <Card
             className="card"
-            style={{ width: "18rem", height: "40rem" }}
+            style={{ width: "25rem", height: "40rem" }}
             key={item._id}
           >
             <Image
+              style={{ width: "25rem", height: "25rem" }}
               variant="top"
               className="img-fluid"
               alt="Product image."
@@ -47,14 +48,18 @@ function ItemsInSingleGenre(props) {
               publicId={item.image_id}
             />
             <Card.Body>
-              <Card.Title class="card-title">{item.name}</Card.Title>
+              <Card.Title class="card-title text-center title">
+                {item.name}
+              </Card.Title>
             </Card.Body>
             <ListGroup className="list-group-flush">
-              <ListGroupItem>{item.location}</ListGroupItem>
-              <ListGroupItem>{item.condition}</ListGroupItem>
+              <ListGroupItem>Location: {item.location}</ListGroupItem>
+              <ListGroupItem>Condition: {item.condition}</ListGroupItem>
             </ListGroup>
             <Card.Body>
-              <Card.Link href="#">More Information</Card.Link>
+              <button type="submit" className="request-btn">
+                More Information
+              </button>
             </Card.Body>
           </Card>
         ))}
